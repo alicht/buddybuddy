845b5aa614aafe10d954bc6276476b52d4456578
Andre Malan <raden.andre@gmail.com>
2014-09-26 11:35:58 -0400

    relationships for pairings

--- app/models/log.js
+++ app/models/log.js
@@ -9,9 +9,9 @@ var Log = DS.Model.extend({
 
 Log.reopenClass({
   FIXTURES: [
-    { id: 1, userId: 1, pairingId: 1,  message: 'Hello world', date: Date.parse("2014/09/01") },
-    { id: 2, userId: 1, pairingId: 1, message: 'Brian does lots of hard things', date: Date.parse("2014/09/02")},
-    { id: 3, userId: 2, pairingId: 1, message: 'Charlie has a very easy job', date: Date.parse("2014/09/02") }
+    { id: 1, user: 1, pairing: 1,  message: 'Hello world', date: Date.parse("2014/09/01") },
+    { id: 2, user: 1, pairing: 1, message: 'Brian does lots of hard things', date: Date.parse("2014/09/02")},
+    { id: 3, user: 2, pairing: 1, message: 'Charlie has a very easy job', date: Date.parse("2014/09/02") }
   ]
 });
 
