20f42552fcd1439839758781f3cda9c25c92a96e
Hyejin Kim <kim.heyjin@gmail.com>
2014-09-26 11:53:00 -0400

    link to paring show

--- app/router.js
+++ app/router.js
@@ -7,9 +7,10 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('users', function() { });
-  this.resource('pairing', {path: 'pairings/:pairing_id'})
-  this.resource('pairings', function() { });
-  this.route('pairings/pairing');
+  
+  this.resource('pairings', function() { 
+  	this.route('pairing', {path: ':pairing_id'})
+  });
 });
 
 export default Router;
