97c0678988196f01f37a350fe28ba77647673f32
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-03 15:50:40 -0400

    more fixture data

--- public/app/models/log.js
+++ public/app/models/log.js
@@ -9,16 +9,16 @@ var Log = DS.Model.extend({
 
 Log.reopenClass({
   FIXTURES: [
-    { id: 1, user: 1, pairing: 1, date: new Date("2014/09/22"), message: 'Hello world'},
-    { id: 2, user: 2, pairing: 1, date: new Date("2014/09/01"), message: 'Brian does lots of hard things'},
-    { id: 3, user: 2, pairing: 1, date: new Date("2014/09/02"), message: 'Charlie has a very easy job'},
-    { id: 4, user: 3, pairing: 2, date: new Date("2014/09/01") },
-    { id: 5, user: 4, pairing: 2, date: new Date("2014/09/02") },
-    { id: 6, user: 1, pairing: 3, date: new Date("2014/09/08") },
-    { id: 7, user: 1, pairing: 3, date: new Date("2014/09/09") },
-    { id: 8, user: 1, pairing: 3, date: new Date("2014/09/10") },
-    { id: 9, user: 1, pairing: 3, date: new Date("2014/09/22") },
-    { id: 10, user: 1, pairing: 3, date: new Date("2014/09/23"), message: 'Talked about Buddy Buddy!' }
+    { id: 1, user: 1, pairing: 1, date: new Date("2014/09/16"), message: 'Hello world'},
+    { id: 2, user: 2, pairing: 1, date: new Date("2014/09/17"), message: 'Brian does lots of hard things'},
+    { id: 3, user: 2, pairing: 1, date: new Date("2014/09/20"), message: 'Charlie has a very easy job'},
+    { id: 4, user: 3, pairing: 2, date: new Date("2014/09/18") },
+    { id: 5, user: 4, pairing: 2, date: new Date("2014/09/19") },
+    { id: 6, user: 1, pairing: 3, date: new Date("2014/09/23") },
+    { id: 7, user: 1, pairing: 3, date: new Date("2014/09/24") },
+    { id: 8, user: 1, pairing: 3, date: new Date("2014/09/25") },
+    { id: 9, user: 1, pairing: 3, date: new Date("2014/09/26") },
+    { id: 10, user: 1, pairing: 3, date: new Date("2014/09/27"), message: 'Talked about Buddy Buddy!' }
   ]
 });
 
