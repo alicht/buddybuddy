e6a715df6f391f1c3fdb40834e52b9fa7fdbd29d
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-03 15:50:29 -0400

    user has many pairings and paring has many users

--- public/app/models/user.js
+++ public/app/models/user.js
@@ -1,16 +1,15 @@
 import DS from 'ember-data';
 
 var User = DS.Model.extend({
-  name: DS.attr(),
-  primaryPairings: DS.hasMany('pairing', {inverse: 'primaryUser'}),
-  secondaryPairings: DS.hasMany('pairing', {inverse: 'secondaryUser'})
+  name: DS.attr('string'),
+  pairings: DS.hasMany('pairing', {async: true})
 });
 
 User.reopenClass({
   FIXTURES: [
-    { id: 1, name: 'Charlie' },
-    { id: 2, name: 'Brian' },
-    { id: 3, name: 'Heyjin' },
+    { id: 1, name: 'Charlie', pairings: [1, 3] },
+    { id: 2, name: 'Brian', pairings: [1, 2] },
+    { id: 3, name: 'Heyjin', pairings: [2, 3] },
     { id: 4, name: 'André' }
   ]
 });
