dc3b2bc288076e91f78986cd9f327024573ca626
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-05 23:41:00 -0400

    separated out rails and ember directory

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,7 @@
 charlie = User.create(name: "Charlie Ridley")
 nick = User.create(name: "Nick Blanchet")
 heyjin = User.create(name: "Designy Heyjin")
-andré = User.create(name: "André Malan")
+andre = User.create(name: "Andre Malan")
 
 pairing = Pairing.create(start_date: DateTime.now - 7.days, end_date: DateTime.now)
 pairing.users << charlie
