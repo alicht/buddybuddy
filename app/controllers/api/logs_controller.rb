79ecaf61a36b71b4afef02120555dacc78f3aecd
Blanchet <me@nblanchet.com>
2014-10-09 16:15:00 -0400

    heyjin :boom:

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -3,7 +3,7 @@ class Api::LogsController < ApplicationController
   def index
     #pairing_id
     @logs = Log.all
-    @logs.select!{|log| log.pairing_id == params[:pairing_id].to_i} if params[:pairing_id]
+    @logs.to_a.select!{|log| log.pairing_id == params[:pairing_id].to_i} if params[:pairing_id]
     render json: @logs
   end
 
