529a65f822f82e89afb3a047069a83cb3877f432
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-09 15:20:26 -0400

    data

--- public/app/models/pairing.js
+++ public/app/models/pairing.js
@@ -12,17 +12,17 @@ var Pairing = DS.Model.extend({
 
 var fixtures = [];
 var dates = [
-  ['2014/10/06', '2014/10/10'],
-  ['2014/10/13', '2014/10/17'],
-  ['2014/10/20', '2014/10/24']
+  ['2014/09/22', '2014/09/26'],
+  ['2014/09/29', '2014/10/03'],
+  ['2014/10/06', '2014/10/10']
 ];
 
 fixtures.push({id: fixtures.length+1, users: [1, 2], logs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], startDate: new Date(dates[0][0]), endDate: new Date(dates[0][1])});
 fixtures.push({id: fixtures.length+1, users: [1, 2], logs: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20], startDate: new Date(dates[1][0]), endDate: new Date(dates[1][1])});
-fixtures.push({id: fixtures.length+1, users: [1, 2], logs: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30], startDate: new Date(dates[2][0]), endDate: new Date(dates[2][1])});
-fixtures.push({id: fixtures.length+1, users: [3,4], logs: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40], startDate: new Date(dates[0][0]), endDate: new Date(dates[0][1])});
-fixtures.push({id: fixtures.length+1, users: [3,4], logs: [41, 42, 43, 44, 45, 46, 47, 48, 49, 50], startDate: new Date(dates[1][0]), endDate: new Date(dates[1][1])});
-fixtures.push({id: fixtures.length+1, users: [3,4], logs: [51, 52, 53, 54, 55, 56, 57, 58, 59, 60], startDate: new Date(dates[2][0]), endDate: new Date(dates[2][1])});
+fixtures.push({id: fixtures.length+1, users: [1, 2], logs: [21, 22, 23, 24, 25, 26], startDate: new Date(dates[2][0]), endDate: new Date(dates[2][1])});
+fixtures.push({id: fixtures.length+1, users: [3,4], logs: [27, 28, 29, 30, 31, 32, 33, 34, 35, 36], startDate: new Date(dates[0][0]), endDate: new Date(dates[0][1])});
+fixtures.push({id: fixtures.length+1, users: [3,4], logs: [37, 38, 39, 40, 41, 42, 43, 44, 45, 46], startDate: new Date(dates[1][0]), endDate: new Date(dates[1][1])});
+fixtures.push({id: fixtures.length+1, users: [3,4], logs: [47, 48, 49, 50, 51, 52], startDate: new Date(dates[2][0]), endDate: new Date(dates[2][1])});
 
 console.log( 'pairings: ', fixtures );
 
