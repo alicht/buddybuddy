1adfb02db1cdbfe4b68c66bf115b1b67b0fc8243
Blanchet <me@nblanchet.com>
2014-10-09 17:56:44 -0400

    switch ember-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@
   "alvin@simplereach.com",
   "csira@simplereach.com"
   ].each do |email|
-    User.create(name: email.split('@').first)
+    User.find_or_create_by(name: email.split('@').first)
   end
 
 Pairing.generate!
