6f0f86a41dd2a157b05cad48c2252c548a02040e
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-09 11:46:01 -0400

    pairing show data

--- app/controllers/api/pairings_controller.rb
+++ app/controllers/api/pairings_controller.rb
@@ -5,6 +5,11 @@ class Api::PairingsController < ApplicationController
     render json: @pairings
   end
 
+  def show
+    @pairing = Pairing.find(params[:id])
+    render json: @pairing
+  end
+
   def by_user_id
     @pairings = User.find(params[:user_id]).pairings
     render json: @pairings
