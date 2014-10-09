a755921ec40e1cf2cb302adef22b865372e22adf
WheellzArmine <zareenmwilhelm@gmail.com>
2014-10-09 15:57:31 -0400

    welcome tests:

---
+++ public/tests/welcome_tests.js
@@ -0,0 +1,26 @@
+module( "Integration - bbWelcome", {
+   setup: function() {
+     // Sr.reset(); 
+     //visit login page: 
+     visit('/pairings/1');  
+   } 
+ });
+
+
+
+//check currentPath 
+
+
+//check welcome message
+
+
+//check date range 
+
+
+//check the pair
+
+
+//check recent history
+
+
+
