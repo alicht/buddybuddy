fb18d9359456613ab0791835c5040f6b7f4f1110
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-10 15:56:49 -0400

    smooth button updates after checking in

--- public/app/controllers/pairings/pairing.js
+++ public/app/controllers/pairings/pairing.js
@@ -4,6 +4,8 @@ export default Ember.ObjectController.extend({
   today: new Date(),
   
   isCheckedIn: function(){
+ 
+
     if (this.get('logs.isFulfilled') && this.get('logs.length') > 0){
       var last = this.get('logs').sortBy('createdAt').get('lastObject');
       return new Date().toDateString()===last.get('createdAt').toDateString();
--- public/app/routes/pairings/pairing.js
+++ public/app/routes/pairings/pairing.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	actions: {
-		checkin: function(pairing){
-			var log = this.store.createRecord('log', {
-				user: this.get('currentUserService.user'),
-				pairing: pairing
-			});
-			log.save();
-		}
-	}
+  actions: {
+    checkin: function(pairing){
+      var route = this;
+
+      var log = this.store.createRecord('log', {
+        user: this.get('currentUserService.user'),
+        pairing: pairing
+      });
+
+      log.save().then(function(l){
+        pairing.get('logs').pushObject(l);
+        route.transitionTo('pairings.pairing', pairing);
+      }); 
+    }
+  }
 });
