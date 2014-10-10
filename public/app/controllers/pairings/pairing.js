1173d29c4487c983b796e2883602b96b81344380
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-10 16:39:35 -0400

    check is me

--- public/app/controllers/pairings/pairing.js
+++ public/app/controllers/pairings/pairing.js
@@ -4,11 +4,9 @@ export default Ember.ObjectController.extend({
   today: new Date(),
   
   isCheckedIn: function(){
- 
-
     if (this.get('logs.isFulfilled') && this.get('logs.length') > 0){
       var last = this.get('logs').sortBy('createdAt').get('lastObject');
-      return new Date().toDateString()===last.get('createdAt').toDateString();
+      return last.get('user.isMe') && new Date().toDateString()===last.get('createdAt').toDateString();
     }else{
       return false;
     }
