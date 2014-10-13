41b24cb36b54c89f8dc573f5fbfa2d0c4e20952e
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-13 10:36:52 -0400

    check check-in today

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,9 @@
 class Log < ActiveRecord::Base
   belongs_to :user
   belongs_to :pairing
+
+  def self.today_log_by_user(user_id)
+    Log.where(["user_id = ? AND created_at >= ?", user_id, Time.now.beginning_of_day])
+  end
+
 end
--- public/app/models/user.js
+++ public/app/models/user.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 var User = DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
+  checkedin: DS.attr('boolean'),
   pairings: DS.hasMany('pairing', {async: true}),
 
   isMe: function(){
