9042caabcb3481bbd054d0284f1482ac1dd82b6e
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-19 16:34:23 -0400

    added log message

--- public/app/routes/pairings/pairing.js
+++ public/app/routes/pairings/pairing.js
@@ -5,10 +5,12 @@ export default Ember.Route.extend({
     checkin: function(pairing){
       var route = this;
       var user = this.get('currentUserService.user');
+      var message = this.get('controller.logMessage');
 
       var log = this.store.createRecord('log', {
         user: user,
-        pairing: pairing
+        pairing: pairing,
+        message: message
       });
 
       log.save().then(function(l){
