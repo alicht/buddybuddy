221224daf6cd5ca059f27f608468a6d055f63a0b
Hyejin Kim <kim.heyjin@gmail.com>
2014-10-30 17:11:16 -0400

    display different message for ended pairing

--- ember/app/controllers/pairings/pairing.js
+++ ember/app/controllers/pairings/pairing.js
@@ -2,4 +2,5 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   logMessage: null,
+  isCheckedInToday: Ember.computed.or('content.isEnded', 'currentUserService.user.checkedin')
 });
