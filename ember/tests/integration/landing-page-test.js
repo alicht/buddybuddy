81f2197110a1d52ce868256fb4c51b9e49d2be38
Hyejin Kim <kim.heyjin@gmail.com>
2014-11-03 23:15:36 -0500

    added Pretender for faking out remote responses

--- ember/tests/integration/landing-page-test.js
+++ ember/tests/integration/landing-page-test.js
@@ -1,18 +1,37 @@
 import Ember from 'ember';
 import startApp from 'buddybuddy/tests/helpers/start-app';
+import Pretender from 'pretender';
 
-var App;
+var App, server;
 
 module('Integration - Landing Page', {
   setup: function() {
     App = startApp();
+    var logs = [{
+        id: 1,
+        user_id: 1,
+        pairing_id: 1,
+        message: null,
+        created_at: "2014-11-03T15:04:58.442Z",
+        links: {
+          favorites: "/api/favorites?log_id=1"
+        }
+      }];
+
+    server = new Pretender(function() {
+      this.get('/api/logs', function(request) {
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({logs: logs})];
+      });
+    });
   },
+
   teardown: function() {
     Ember.run(App, 'destroy');
+    server.shutdown();
   }
 });
 
-test('', function(){
+test('smoke test', function(){
   visit('/');
   andThen(function(){
     equal(find('header').length, 1);
@@ -22,3 +41,10 @@ test('', function(){
   });
 });
 
+test('display log list', function(){
+  visit('/');
+
+  andThen(function(){
+    equal(find('section.checkins article').length, 1);
+  });
+});
