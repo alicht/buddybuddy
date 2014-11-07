b917a8bc6fec0bb721a34ec8fab8e8aa95ecee96
HeyjinJS <kim.heyjin@gmail.com>
2014-11-07 15:29:56 -0500

    added favorite api

---
+++ app/controllers/api/favorites_controller.rb
@@ -0,0 +1,18 @@
+class Api::FavoritesController < ApplicationController
+
+  def index
+    render json: Favorite.where(log_id: params[:log_id])
+  end
+
+  def create
+    fav = Favorite.create(params.require(:favorite).permit!)
+    render json: fav
+  end
+
+  def destroy
+    #Favorite.find_by_id(params[:id]).try(:destroy)
+    #render status: 204, nothing: true
+    render status: 204, nothing: true
+  end
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :pairings
   has_many :logs
+  has_many :logs
 
   def hide!
     update_attribute(visible: false)
