fae1cbe84ba0d486aa23bd72d08d45c32f8d4c9b
HeyjinJS <kim.heyjin@gmail.com>
2014-11-08 02:01:57 -0500

    limit 30 for buddfeed

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -1,15 +1,15 @@
 class Api::LogsController < ApplicationController
 
   def index
-    #pairing_id
-    @logs = Log.order('created_at DESC')
-    @logs.to_a.select!{|log| log.pairing_id == params[:pairing_id].to_i} if params[:pairing_id]
-    render json: @logs
+    limit = 30
+    logs = Log.order('created_at DESC').limit(limit)
+    logs.to_a.select!{|log| log.pairing_id == params[:pairing_id].to_i} if params[:pairing_id]
+    render json: logs
   end
 
   def create
-    @log = Log.create(params.require(:log).permit!)
-    render json: @log
+    log = Log.create(params.require(:log).permit!)
+    render json: log
   end
 
 end
