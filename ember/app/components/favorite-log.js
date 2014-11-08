fe9ebea4f3f6a0f17400e969b734e058396464e6
HeyjinJS <kim.heyjin@gmail.com>
2014-11-07 22:07:16 -0500

    added favorite(s) in pairing page

--- ember/app/components/favorite-log.js
+++ ember/app/components/favorite-log.js
@@ -3,14 +3,27 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   favorited: function(){
     var userId = this.get('currentUser.id');
-    return this.get('favorites').any(function(fav){
+    return this.get('content.favorites').any(function(fav){
       return fav.get('strUserId') === userId;
     });
-  }.property('favorites.@each.user'),
+  }.property('content.favorites.@each.user'),
 
   actions: {
-    favorite: function(log){
-      this.sendAction('action', log, this.get('favorited'));
+    doFavorite: function(log){
+      var favorite;
+      var currentUser = this.get('currentUser');
+      if (this.get('favorited')){
+        favorite = log.get('favorites').findBy('strUserId', currentUser.get('id'));
+        favorite.destroyRecord().then(function(){
+          log.get('favorites').popObject(favorite);
+        });
+      }else{
+        favorite = this.store.createRecord('favorite', {user: currentUser, log: log});
+        favorite.save().then(function(fav){
+          fav.set('user', currentUser);
+          log.get('favorites').pushObject(fav);
+        }); 
+      }
     },
 
     toggleList: function(){
--- ember/app/routes/index.js
+++ ember/app/routes/index.js
@@ -5,25 +5,5 @@ import AuthRoute from '../mixins/auth-route';
 export default Ember.Route.extend(AuthRoute, {
   model: function(){
     return this.store.find('log');
-  },
-
-  actions: {
-    favorite: function(log, isDelete){
-      var fav;
-      var currentUser = this.get('currentUserService.user');
-
-      if (isDelete){
-        fav = log.get('favorites').findBy('strUserId', currentUser.get('id'));
-        fav.destroyRecord().then(function(){
-          log.get('favorites').popObject(fav);
-        });
-      }else{
-        fav = this.store.createRecord('favorite', {user: currentUser, log: log});
-        fav.save().then(function(f){
-          f.set('user', currentUser);
-          log.get('favorites').pushObject(f);
-        }); 
-      }
-    }
   }
 });
