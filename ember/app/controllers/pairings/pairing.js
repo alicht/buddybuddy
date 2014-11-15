5b17a37194d8f41f02ee4f1ac080b3f8b02ea46d
HeyjinJS <kim.heyjin@gmail.com>
2014-11-14 23:53:51 -0500

    sort logs in pairing page

--- ember/app/controllers/pairings/pairing.js
+++ ember/app/controllers/pairings/pairing.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   logMessage: null,
   isCheckedInToday: Ember.computed.or('content.isEnded', 'session.currentUser.checkedin'),
+  logsSorting: ['createdAt:desc'],
+  sortedLogs: Ember.computed.sort('logs', 'logsSorting'),
   hasMe: function(){
     if(this.get('buddies.length')){
       return this.get('buddies').mapProperty('id').contains(this.get('content.session.currentUser.id'));
