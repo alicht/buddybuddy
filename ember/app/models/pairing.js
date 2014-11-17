37b703fe5a3ce099ee6e8dff6ff4de6353b4d569
HeyjinJS <kim.heyjin@gmail.com>
2014-11-16 22:23:46 -0500

    deployment commit

--- ember/app/models/pairing.js
+++ ember/app/models/pairing.js
@@ -22,8 +22,7 @@ var Pairing = DS.Model.extend({
       return this.get('users').map(function(user, i) {
         return {
           name: user.get('name'),
-          id: user.get('id'),
-          heart: i < length 
+          id: user.get('id')
         };
       });
     }
